c9ccdb595b48ef94ddf1eb8cb60789dc4c38c80b
unknown <huuphuc.0261993@gmail.com>
2019-09-03 11:03:47 +0700

    push folder

---
+++ java.js
@@ -0,0 +1,11 @@
+let inputwidth;
+let inputheight;
+
+inputwidth = prompt("enter the width");
+inputheight = prompt("enter the hight");
+
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+
+let area = width * height;
+document.write("The area is" + " " + area);
